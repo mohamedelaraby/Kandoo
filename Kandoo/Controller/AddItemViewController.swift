1c006bdf868c2a47804a2f9b2b135b4237d5cd1a
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-07-18 17:33:02 +0200

    Add item view controller

---
+++ Kandoo/Controller/AddItemViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddItemViewControllerTableViewController.swift
+//  Kandoo
+//
+//  Created by Winston on 7/18/19.
+//  Copyright © 2019 Winston. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Turn off the large title.
+        navigationItem.largeTitleDisplayMode = .never
+    }
+
+ 
+
+}
